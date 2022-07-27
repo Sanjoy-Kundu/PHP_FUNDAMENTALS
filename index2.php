3e43192cb4530257e7d5ee84c9dc438a095f4901
sanjoy <sanjoykundu187@gamil.com>
2022-07-28 00:50:25 +0600

    added form

--- index2.php
+++ index2.php
@@ -6,18 +6,18 @@ if(isset($_POST["submit"]) == "POST"){
 	if(empty($_POST["name"])){
 		echo "<span style='color:red'>Name field is required</span><br/>";
 	}else{
-		echo "Your name: ".$_POST["name"];
+		echo "Your name: ".$_POST["name"]."<br/>";
 	}
 	if(empty($_POST["email"])){
 		echo "<span style='color:red'>Email Field is Required</span> <br/>";
 	}else{
-		echo  "Your email ".$_POST["email"];
+		echo  "Your email ".$_POST["email"]."<br/>";
 	}
 
 	if(empty($_POST["address"])){
 		echo "<span style='color:red'>Address field is required</span> <br/>";
 	}else{
-		echo  "Your Address ".$_POST["address"];
+		echo  "Your Address ".$_POST["address"]."<br/>";
 	}
 	
 	if(empty($_POST["date"])){
@@ -29,22 +29,22 @@ if(isset($_POST["submit"]) == "POST"){
 		$todayDateNumber = (int)$todaysDate;
 		$perfectAge = $todayDateNumber - $inputDateNumber;
 		if($perfectAge < 18){
-			echo "You are not Adult";
+			echo "You are not Adult"."<br/>";
 		}else{
-			echo "Your age is $perfectAge";
+			echo "Your age is $perfectAge"."<br/>";
 		}
 	}
 
 	if(empty($_POST["gender"])){
 		echo "<span style='color:red'>Please select a option</span> <br/>";
 	}else{
-		echo  "You are ".$_POST["gender"];
+		echo  "You are ".$_POST["gender"]."<br/>";
 	}
 
 	if(empty($_POST["number"])){
 		echo  "<span style='color:red'>This field is required</span>";
 	}else{
-		echo "Your Mobile Number is".$_POST["number"];
+		echo "Your Mobile Number is".$_POST["number"]."<br/>";
 	}
 }
 
